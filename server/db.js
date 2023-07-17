920459b41f9cd3db5f325c9ec57c6992d7a99162
Hady Mashhour <hady.mashhour@gmail.com>
2023-07-18 01:39:39 +0300

    updated db.js to create a container for market data and removed old commented out code

--- server/db.js
+++ server/db.js
@@ -7,8 +7,6 @@ const endpoint = process.env.COSMOS_ENDPOINT;
 
 // Set Database name and container name with unique timestamp
 const databaseName = `StockTradingGame`;
-// const containerName = `StockTradingGame_${timeStamp}`;
-// const partitionKeyPath = ['/categoryId'];
 
 // Authenticate to Azure Cosmos DB
 const cosmosClient = new CosmosClient({ endpoint, key });
@@ -19,11 +17,14 @@ const { database } = await cosmosClient.databases.createIfNotExists({
 });
 console.log(`${database.id} database ready`);
 
-// // Create container if it doesn't exist
-// const { container } = await database.containers.createIfNotExists({
-//   id: containerName,
-//   partitionKey: {
-//     paths: partitionKeyPath,
-//   },
-// });
-// console.log(`${container.id} container ready`);
+// Create containers if they don't exist
+const { container: marketDataContainer } =
+  await database.containers.createIfNotExists({
+    id: 'MarketData',
+    partitionKey: {
+      paths: ['/symbol'],
+    },
+  });
+console.log(`${marketDataContainer.id} container ready`);
+
+// TODO: add active games container
