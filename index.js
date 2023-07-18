c442fcab67d405af35f5f2278e51d1d97a7caa84
Emad Nawar <emadnawar2009@gmail.com>
2023-07-18 21:11:55 +0300

    aa

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const stocks = [
-  { stockName: "apple", stockQuantityInMarket: 9, stockPricePerUnit: 3.55 },
-  { stockName: "google", stockQuantityInMarket: 20, stockPricePerUnit: 8.44 },
-  { stockName: "microsoft", stockQuantityInMarket: 17, stockPricePerUnit: 7.42 }
+  { stockName: 'apple', stockQuantityInMarket: 9, stockPricePerUnit: 3.55 },
+  { stockName: 'google', stockQuantityInMarket: 20, stockPricePerUnit: 8.44 },
+  {
+    stockName: 'microsoft',
+    stockQuantityInMarket: 17,
+    stockPricePerUnit: 7.42,
+  },
 ];
 const players = [
-  { playerName: "AI", stocksInPossession: {typeOfStock: stocks[1], quantityInPossession: 3}, playerMoney: 100 },
+  {
+    playerName: 'AI',
+    stocksInPossession: { typeOfStock: stocks[1], quantityInPossession: 3 },
+    playerMoney: 100,
+  },
 ];
 
 app.use(express.json());
 
-app.post("/api/players", (req, res) => {
+app.post('/api/players', (req, res) => {
   const newPlayer = {
     playerName: req.body.playerName,
     stocksInPossession: new Array(),
     playerMoney: 100,
   };
-  
+
   players.push(newPlayer);
   console.log(players);
   res.send(newPlayer);
