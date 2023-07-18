d8c170196e74656476ae6fef5a1ed5c75d4d21b4
Amira El-Garf <elgarfamira4@gmail.com>
2023-07-19 00:22:32 +0300

    buying action

--- index.js
+++ index.js
@@ -20,18 +20,61 @@ const players = [
 
 app.use(express.json());
 
-app.post('/api/players', (req, res) => {
-  const newPlayer = {
+//for testing
+const containerName = 'players';
+const container = database.container(containerName);
+
+app.post('/api/players', async (req, res) => {
+  const newPlayer = ({
     playerName: req.body.playerName,
-    stocksInPossession: new Array(),
+    stocksInPossession: [],
     playerMoney: 100,
-  };
+  });
+//for testing the buy action
+  const { resource: createdPlayer } = await container.items.create(player);
 
   players.push(newPlayer);
   console.log(players);
   res.send(newPlayer);
 });
 
+
+// Handle buying stocks
+app.post('/api/buy', async (req, res) => {
+  const { playerName, stockName, quantityInPossession } = req.body;
+  const price = getStockPrice(stockName);
+
+  const { resource: player } = await container.item(playerName).read();
+
+  if (player.playerMoney < price) {
+    res.status(400).send('Insufficient funds');
+    return;
+  }
+
+  // Update player's balance
+  player.playerMoney -= price;
+
+  const stock = player.stocksInPossession.find((s) => s.stockName === stockName);
+
+  if (stock) {
+    stock.quantityInPossession += quantityInPossession;
+  } else {
+    player.stocksInPossession.push({ stockName, price, quantity });
+  }
+
+  //const { resource: updatedPlayer } = await container.item(player.id).replace(player);
+  console.log("Stocks bought successfully");
+
+  res.send('Stocks bought successfully');
+});
+
+function getStockPrice(stockName) {
+  //just for testing
+  return 1;
+  //return stockName.stockPricePerUnit;
+  
+  //return Math.floor(Math.random() * 151) + 50;
+}
 //app.post("/api/sell", (req, res) => {
 //  const stocksToBeSold;
 //});
--- playersDB.js
+++
@@ -1,4 +0,0 @@
-const players = [
-    { playerName: "AI", stocksInPossession: stocks[1], playerMoney: 100 },
-  ];
-  
