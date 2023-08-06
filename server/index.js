cadddd3e305197445833a53919a4d390827f6a09
Hady Mashhour <hady.mashhour@gmail.com>
2023-08-07 00:06:20 +0300

    completed the update api

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import * as db from './db.js';
 import express from 'express';
 //import * as game from '../game.mjs';
-import { GameSettings } from '../game.mjs';
+import { Game, GameSettings, GameState } from '../game.mjs';
 import { makeDecision, AIDecision } from './ai.js';
 
 const app = express();
@@ -453,6 +453,163 @@ app.post('/sell', async (req, res) => {
       message: 'Failed to sell stock',
       error: err.message,
     });
+  }
+});
+
+app.get('/update', async (req, res) => {
+  try {
+    const gameId = req.body.gameId;
+    const playerId = req.body.playerId;
+    if (!gameId) {
+      res.status(400).json({
+        error: 'Missing game id',
+      });
+      return;
+    }
+
+    if (!playerId) {
+      res.status(400).json({
+        error: 'Missing player id',
+      });
+      return;
+    }
+
+    const { statusCode, resource } = await db.getGame(gameId);
+    if (statusCode !== 200) {
+      console.log();
+      res.status(statusCode).json({
+        error: resource,
+      });
+      return;
+    }
+
+    const game = Game.fromObject(resource);
+
+    let response = {};
+
+    if (game.state === GameState.waiting) {
+      response = {
+        gameState: game.state,
+        players: Object.keys(game.players)
+          .filter((id) => id !== playerId)
+          .map((playerId) => {
+            return {
+              id: playerId,
+              name: game.players[playerId].name,
+              netWorth: game.players[playerId].money,
+            };
+          }),
+        hostId: game.hostId,
+      };
+    } else if (game.state === GameState.active) {
+      // TODO: check for end conditions
+      // TODO: send ai net worth along with players
+      const dayNumber = Math.floor(
+        (Date.now() - game.startTimestamp) /
+          1000 /
+          game.settings.roundDurationSeconds
+      );
+      const stockData = new Array(dayNumber);
+
+      console.log('day number', dayNumber);
+
+      // for each symbol
+      for (let i = 0; i < Object.keys(game.stockStartIds).length; i++) {
+        const symbol = Object.keys(game.stockStartIds)[i];
+        console.log('starting', symbol);
+        // get prices from start id to current day + a buffer of 20 days
+        const rawEntries = await db.getMarketDataEntries(
+          symbol,
+          game.stockStartIds[symbol],
+          dayNumber + 20
+        );
+        console.log('raw entries', rawEntries);
+        // for each day
+        for (let j = 0; j < rawEntries.length; j++) {
+          const entry = rawEntries[j];
+          if (entry.statusCode !== 200) {
+            throw new Error(entry.resourceBody);
+          }
+
+          const body = entry.resourceBody;
+          if (!stockData[j]) {
+            stockData[j] = {};
+          }
+          // add anonymised data to stock data
+          stockData[j][symbol] = {
+            id: j,
+            symbol: symbol,
+            price: body.price,
+            volume: body.volume,
+          };
+        }
+      }
+
+      console.log('stock data', stockData);
+
+      response = {
+        gameState: game.state,
+        players: [
+          ...Object.keys(game.players)
+            .filter((id) => id !== playerId)
+            .map((playerId) => {
+              // calculate player's net worth
+              let netWorth = game.players[playerId].money;
+              for (
+                let i = 0;
+                i < Object.keys(game.players[playerId].stocks).length;
+                i++
+              ) {
+                const symbol = Object.keys(game.players[playerId].stocks)[i];
+                const quantity = game.players[playerId].stocks[symbol];
+                const price = stockData[20 + dayNumber];
+
+                netWorth += quantity * price;
+              }
+
+              return {
+                id: playerId,
+                name: game.players[playerId].name,
+                netWorth: netWorth,
+              };
+            }),
+          {
+            id: 'ai',
+            name: 'AI',
+            // TODO: test if day number starts at 0 or 1 (i believe it starts at 0 because of the math floor)
+            netWorth: resource.aiNetWorthOverTime[dayNumber],
+          },
+        ],
+        player: game.players[playerId],
+        stockData: stockData,
+      };
+      console.log('response', response);
+    } else if (game.state === GameState.finished) {
+      let winner = game.players[Object.keys(game.players)[0]];
+      for (let i = 0; i < Object.keys(game.players).length; i++) {
+        const playerId = Object.keys(game.players)[i];
+        if (game.players[playerId].money > winner.money) {
+          winner = game.players[playerId];
+        }
+      }
+      response = {
+        gameState: game.state,
+        winner: winner,
+        losers: Object.keys(game.players)
+          .filter((id) => id !== winner.id)
+          .map((playerId) => {
+            return game.players[playerId];
+          }),
+      };
+    }
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: error.message,
+    });
+    return;
   }
 });
 
