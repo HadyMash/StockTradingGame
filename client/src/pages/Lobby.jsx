99d2286ac31c24e40bfbbcce06d635abec8a7aa2
Emad Nawar <emadnawar2009@gmail.com>
2023-08-08 20:07:19 +0300

    add crown to host player

--- client/src/pages/Lobby.jsx
+++ client/src/pages/Lobby.jsx
@@ -205,6 +205,9 @@ function Lobby() {
                 name={player.name}
                 handleKick={() => handleKick(player)}
                 key={player.id}
+                isHost={
+                  game.hostId === localPlayer.id
+                }
                 showKick={
                   game.hostId === localPlayer.id && player.id !== localPlayer.id
                 }
@@ -238,12 +241,12 @@ function Lobby() {
   );
 }
 
-// TODO: add host icon (crown)
-function Player({ name, handleKick, showKick = true }) {
+function Player({ name, handleKick, isHost = true, showKick = true}) {
   Player.propTypes = {
     name: PropTypes.string.isRequired,
     handleKick: PropTypes.func.isRequired,
     showKick: PropTypes.bool,
+    isHost: PropTypes.bool,
   };
 
   return (
@@ -258,6 +261,11 @@ function Player({ name, handleKick, showKick = true }) {
           marginTop: '12px',
         }}
       >
+        {isHost &&
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns: xlink="http://www.w3.org/1999/xlink" fill="#ffcc00" height="20px" width="20px" version="1.1" id="Capa_1" viewBox="0 0 220 220" xml: space="preserve" style={{ 'marginRight': '5px' }}>
+            <path d="M220,98.865c0-12.728-10.355-23.083-23.083-23.083s-23.083,10.355-23.083,23.083c0,5.79,2.148,11.084,5.681,15.14  l-23.862,21.89L125.22,73.002l17.787-20.892l-32.882-38.623L77.244,52.111l16.995,19.962l-30.216,63.464l-23.527-21.544  c3.528-4.055,5.671-9.344,5.671-15.128c0-12.728-10.355-23.083-23.083-23.083C10.355,75.782,0,86.137,0,98.865  c0,11.794,8.895,21.545,20.328,22.913l7.073,84.735H192.6l7.073-84.735C211.105,120.41,220,110.659,220,98.865z" />
+          </svg>
+        }
         <p
           style={
             showKick
